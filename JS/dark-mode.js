d72556f915736115e96f9d0f42a27f26617c180a
Leandro-Pinho <leandroessencia@gmail.com>
2023-05-26 22:41:37 -0300

    adicionando o html e css e função light e dark com JS

---
+++ JS/dark-mode.js
@@ -0,0 +1,20 @@
+const btnDark = document.querySelector('.btn-dark-mode')
+
+btnDark.addEventListener('click', () => {
+    // é uma função boleana, toda vez que clicar vai mudar independente de qual esteja.
+    document.body.classList.toggle('dark-mode');
+
+    // para mudar o butao de acordo com o tema
+    if (document.body.classList === 'dark-mode') {
+        btnDark.innerHTML = `
+        <i class="fa-regular fa-sun"></i>
+        Light Mode
+        `
+    } else {
+        btnDark.innerHTML = `
+        <i class="fa-regular fa-moon"></i>
+        Dark Mode
+        `
+    }
+
+})
